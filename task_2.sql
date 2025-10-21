b936b65c2c4cb60d877d944cc9114d760eff5416
Sadorra <rodiawg@gmail.com>
2025-10-21 17:24:09 +0300

    fix: fix requirement fail

--- task_2.sql
+++ task_2.sql
@@ -37,7 +37,7 @@ CREATE TABLE Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     unit_price DECIMAL(10, 2),
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
